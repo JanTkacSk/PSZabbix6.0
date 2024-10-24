c0924226613f161e45db3632428080f8a682dc73
JanTkacSK <jan.tkac.sk@gmail.com>
2024-10-24 22:50:36 +0200

    commit

--- Core-Private/ZX.UpdatePSProfile.ps1
+++ Core-Private/ZX.UpdatePSProfile.ps1
@@ -1,3 +1,8 @@
 $Content = Get-Content $Profile | Where-Object {$_ -notmatch "ZX.ImportPSZabbix6.0.ps1"}
 $FirstLine = ". $PSScriptRoot.\ZX.ImportPSZabbix6.0.ps1"
 @($FirstLine) + $Content | Set-Content $PROFILE
+$AddedLine = Get-Content $Profile | Where-Object {$_ -eq $FirstLine}
+if ($AddedLine -ne $null){
+    Write-Host -ForegroundColor Green "PowerShell profile was updated"
+}
+pause
