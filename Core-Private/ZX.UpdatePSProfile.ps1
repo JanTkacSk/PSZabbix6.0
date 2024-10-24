c89f84a2778a3182287c6bc135877b24f9b02a95
JanTkacSK <jan.tkac.sk@gmail.com>
2024-10-24 22:34:33 +0200

    commit

--- Core-Private/ZX.UpdatePSProfile.ps1
+++ Core-Private/ZX.UpdatePSProfile.ps1
@@ -1,3 +1,3 @@
-$Content = Get-Content $Profile | Where-Object {$_ -notmatch "PSZabbix6.0.ps1"}
-$FirstLine = ". $PSScriptRoot..\ZX.ImportPSZabbix6.0.ps1"
+$Content = Get-Content $Profile | Where-Object {$_ -notmatch "ZX.ImportPSZabbix6.0.ps1"}
+$FirstLine = ". $PSScriptRoot.\ZX.ImportPSZabbix6.0.ps1"
 @($FirstLine) + $Content | Set-Content $PROFILE
