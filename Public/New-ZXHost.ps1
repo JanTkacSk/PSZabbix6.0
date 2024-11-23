0100ac611fa8295f4a2cfd982258859f0df9407e
JanTkacSK <jan.tkac.sk@gmail.com>
2024-11-23 17:58:38 +0100

    commit

--- Public/New-ZXHost.ps1
+++ Public/New-ZXHost.ps1
@@ -15,13 +15,14 @@ function New-ZXHost {
 
     #Basic PS Object wich will be edited based on the used parameters and finally converted to json
     $PSObj = [PSCustomObject]@{
+        "host"=$Name;
         "jsonrpc" = "2.0";
         "method" = "host.create";
         "params" = [PSCustomObject]@{
-            "groups"=$HostGroup;
-            "templates"=$Template;
-            "macros"=$Macros;
-            "interfaces"=$Interfaces
+            "groups"=[array]$HostGroup;
+            "templates"=[array]$Template;
+            "macros"=[array]$Macros;
+            "interfaces"=[array]$Interfaces
         }; 
         #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
         "auth" = "test" #[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken)));
@@ -34,10 +35,13 @@ function New-ZXHost {
         #Show JSON Request if -ShowJsonRequest switch is used
         If ($ShowJsonRequest -or $WhatIf){
             Write-Host -ForegroundColor Yellow "JSON REQUEST"
-            $PSObjShow = $PSObj
+            $PSObjShow = $PSObj | ConvertTo-Json | ConvertFrom-Json  
             $PSObjShow.auth = "*****"
             $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
             Write-Host -ForegroundColor Cyan $JsonShow
         }
+
+        #Add templates to the basic PSObject
+
 
 }
